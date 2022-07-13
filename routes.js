b5d1c8fb29657c3a884100dfbe929b80f6fd6a9e
ittttori01 <ittttori01@gmail.com>
2022-07-13 16:59:06 +0900

    route

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const Router = app => {
 
     const SignUpControllers = require('./controllers/signup/signUp');
-    app.post('signup/send/code',(req,res) => SignUpControllers.sendDigitCode(req,res));
+    app.post('/signup/send/code',(req,res) => SignUpControllers.sendDigitCode(req,res));
     app.post('/signup/check/code',(req,res)=> SignUpControllers.checkDigitCode(req,res));
     app.post('/check/email',(req,res)=> SignUpControllers.checkEmail(req,res));
     app.post('/signup',(req,res)=> SignUpControllers.signUp(req,res));
@@ -11,6 +11,11 @@ const Router = app => {
 
     const Mypage = require('./controllers/mypage/index');
     app.post('/mypage',(req,res) => Mypage.getUserInfo(req,res));
+
+    const ResetPassword = require('./controllers/resetPassword/resetPassword');
+    app.post('/reset/send/code',(req,res) => ResetPassword.sendCode(req,res));
+    app.post('/reset/check/code',(req,res) => ResetPassword.checkDigitCode(req,res));
+    app.put('/reset/pwd',(req,res) => ResetPassword.setNewPassword(req,res));
 };
 
 module.exports = Router;
